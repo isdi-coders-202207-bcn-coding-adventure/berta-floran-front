90ee50ba4e4c2f5473a3e9b51dda3385cc5b044f
Berta Floran <bertafloran@hotmail.com>
2022-08-22 13:57:31 +0200

    create html for counter

---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,18 @@
+const Counter = (): JSX.Element => {
+  return (
+    <>
+      <div className="container">
+        <span>days</span>
+        <div className="counter1">2</div>
+        <span>hours</span>
+        <div className="counter2">20</div>
+        <span>minutes</span>
+        <div className="counter3">34</div>
+        <span>seconds</span>
+        <div className="counter4">09</div>
+      </div>
+    </>
+  );
+};
+
+export default Counter;
