e4721b4a4d2041774b9ef29ac8d542bab20d1f10
Berta Floran <bertafloran@hotmail.com>
2022-08-24 17:27:34 +0200

    add Data and change function

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,50 +1,58 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import dateline from "../../data/date";
 import CounterStyled from "./CounterStyled";
 
-const Counter = (): JSX.Element => {
-  interface ICountDown {
-    days: number;
-    hours: number;
-    minutes: number;
-    seconds: number;
-  }
-  const initialTime: ICountDown = {
-    days: 31,
-    hours: 24,
-    minutes: 60,
-    seconds: 60,
-  };
-  const [state, setState] = useState<ICountDown>(initialTime);
+const getTimeRemaining = (dateline: Date) => {
+  const total = dateline.getTime() - new Date().getTime();
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
 
-  setInterval(() => {
-    const date = new Date();
-    const newDate: ICountDown = {
-      days: initialTime.days - date.getDay(),
-      hours: initialTime.hours - date.getHours(),
-      minutes: initialTime.minutes - date.getMinutes(),
-      seconds: initialTime.seconds - date.getSeconds(),
-    };
-    setState(newDate);
-  }, 1000);
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds,
+  };
+};
+
+const Counter = (): JSX.Element => {
+  const initialTime = {
+    total: 0,
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  };
+
+  const [dateState, setDateState] = useState(initialTime);
+
+  useEffect(() => {
+    setInterval(() => {
+      setDateState(getTimeRemaining(dateline));
+    }, 1000);
+  }, []);
 
   return (
     <>
       <CounterStyled>
         <div className="container">
           <div className="counter">
-            <span className="counter__number">{state.days}</span>
+            <span className="counter__number">{dateState.days}</span>
             <span className="counter__label">days</span>
           </div>
           <div className="counter">
-            <span className="counter__number">{state.hours}</span>
+            <span className="counter__number">{dateState.hours}</span>
             <span className="counter__label">hours</span>
           </div>
           <div className="counter">
-            <span className="counter__number">{state.minutes}</span>
+            <span className="counter__number">{dateState.minutes}</span>
             <span className="counter__label">minutes</span>
           </div>
           <div className="counter">
-            <span className="counter__number">{state.seconds}</span>
+            <span className="counter__number">{dateState.seconds}</span>
             <span className="counter__label">seconds</span>
           </div>
         </div>
---
+++ src/data/date.ts
@@ -0,0 +1,3 @@
+const dateline: Date = new Date("2023-8-24");
+
+export default dateline;
